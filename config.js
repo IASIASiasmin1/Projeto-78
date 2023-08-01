dc3a9906bab2116ab393925d1ca79d6b6a37db36
IaSm1N <113642508+IASIASiasmin1@users.noreply.github.com>
2023-08-01 17:47:04 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase" 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvA-pTjXAxt4--2SMtKGku6_A-ibsb2pY",
+  authDomain: "ciclista-eletronico-20679.firebaseapp.com",
+  projectId: "ciclista-eletronico-20679",
+  storageBucket: "ciclista-eletronico-20679.appspot.com",
+  messagingSenderId: "947107969053",
+  appId: "1:947107969053:web:15c662ec4c78f73215aef4"
+};
+
+
+  firebase.initializeApp(firebaseConfig);
+  
+  export default firebase.firestore();
+  
